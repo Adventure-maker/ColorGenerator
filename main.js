bf7fa92edb6e95e05028eb03d5341ef5d3f01395
Pyami <1pyami@gmail.com>
2021-04-11 15:49:57 +0530

    first commit

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const bacground = document.getElementById('background')
+const bacground = document.getElementById('background');
 
 let colorsArr = [
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'
-]
+];
 
 window.addEventListener('keypress', e => {
     if (e.keyCode == 32) {
